4fe0cd6d00b6ec293cf1f126a719d8ec5a2ccec1
Thedarkness-bd <toisiftasrik@gmail.com>
2024-03-23 17:53:52 +0600

    Third file added

---
+++ third.java
@@ -0,0 +1,19 @@
+public class third {
+    public static void main(String[] args) {
+        // Data types
+        // primitive types { byte , short , char , boolean, int, long,double,float}
+        int a = 5;
+        char c = 'c';
+        boolean b = true;
+        double d = 45.77888;
+        float f = 56;
+
+        // printing values;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(f);
+
+    }
+}
