ba9d3b8496718d3a65ab678556fc909e12c6ea6d
Thedarkness-bd <toisiftasrik@gmail.com>
2024-03-23 17:53:20 +0600

    Second file added

---
+++ Second.java
@@ -0,0 +1,15 @@
+public class Second {
+    public static void main(String[] args) {
+        // variables
+        int a = 10, b = 5;
+        System.out.println(a);
+        System.out.println(b);
+
+        String name = "Tousif Tasrik";
+        System.out.println(name);
+
+        // changing value
+        a = 50;
+        System.out.println(a);
+    }
+}
