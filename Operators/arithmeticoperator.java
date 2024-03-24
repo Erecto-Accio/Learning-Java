951b04784f346c366d5d1d42c795c5d87498f599
Thedarkness-bd <toisiftasrik@gmail.com>
2024-03-24 09:04:14 +0600

    Arithmetic operator done

---
+++ Operators/arithmeticoperator.java
@@ -0,0 +1,21 @@
+public class arithmeticoperator {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+
+        System.out.println("Addition: " + (a + b));
+        System.out.println("Subtraction: " + (a - b));
+        System.out.println("Multiplication: " + (a * b));
+        System.out.println("Division: " + (a / b));
+        System.out.println("Modulo: " + (a % b));
+
+        /*
+         * Let's learn modulo in depth
+         * Take a number which is 17 . Now we are gonna divide 17 by 3 . We all know we
+         * can divide 17 by 3 which is 5. Because 15 is the most nearest integer. Now if
+         * we subtract 15 from the answer we get 2. That is our answer
+         * 
+         * In code 10 % 5 means 5 * 2 = 10 . 10 - 10 = 0 so the 0 is the answer
+         */
+    }
+}
