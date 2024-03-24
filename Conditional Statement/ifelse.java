1e197205a1a0ec6e743924e7c64dbc95373bf830
Thedarkness-bd <toisiftasrik@gmail.com>
2024-03-24 21:15:39 +0600

    First condition code comitted

---
+++ Conditional Statement/ifelse.java
@@ -0,0 +1,11 @@
+public class ifelse {
+    public static void main(String[] args) {
+        int age = 26;
+
+        if (age >= 18) {
+            System.out.println("You can vote");
+        } else {
+            System.out.println("You cannot vote");
+        }
+    }
+}
