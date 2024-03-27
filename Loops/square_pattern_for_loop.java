592424fc4299b45da206419396c9b41b19e13e34
Thedarkness-bd <toisiftasrik@gmail.com>
2024-03-27 22:26:48 +0600

    square pattern using for loop added

---
+++ Loops/square_pattern_for_loop.java
@@ -0,0 +1,7 @@
+public class square_pattern_for_loop {
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {
+            System.out.println("* * * * *");
+        }
+    }
+}
