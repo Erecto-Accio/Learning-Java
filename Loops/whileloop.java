571ca0dbf191b453063cefa8362a41612ec78d66
Thedarkness-bd <toisiftasrik@gmail.com>
2024-03-27 17:52:54 +0600

    While loop added

---
+++ Loops/whileloop.java
@@ -0,0 +1,19 @@
+public class whileloop {
+    public static void main(String[] args) {
+        /*
+         * 
+         * While Loop
+         * 
+         * syntax : while(condition){
+         * // statement
+         * }
+         * 
+         */
+
+        int counter = 0;
+        while (counter < 10) {
+            System.out.println(counter);
+            counter++;
+        }
+    }
+}
