014f460291b78d821a41408b8a1d1086eea08804
Thedarkness-bd <toisiftasrik@gmail.com>
2024-03-27 22:21:35 +0600

    For loop basic syntax and basic code added

---
+++ Loops/forloop.java
@@ -0,0 +1,17 @@
+public class forloop {
+    public static void main(String[] args) {
+        /*
+         * Basic Syntax of for loop
+         * 
+         * for(initialization; condition; updation){
+         * 
+         * // do something
+         * 
+         * }
+         */
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Hello,World"); // prints Hello,World 10 times
+        }
+    }
+}
