e8ad4c582a74a9a89c853b8beacf0371ec488fec
Thedarkness-bd <toisiftasrik@gmail.com>
2024-04-01 22:40:37 +0600

    calculate sum with returned value

---
+++ function and methods/return_sum.java
@@ -0,0 +1,10 @@
+public class return_sum {
+    public static int calculateSum(int a, int b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        int result = calculateSum(5, 10);
+        System.out.println("Sum is: " + result);
+    }
+}
