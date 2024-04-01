ae193675d084f5634d9a77f42e1e615690ba7340
Thedarkness-bd <toisiftasrik@gmail.com>
2024-04-01 22:35:11 +0600

    basic function with parameter added

---
+++ function and methods/function_with_parameter.java
@@ -0,0 +1,11 @@
+public class function_with_parameter {
+    // defining the function with parameter
+    public static void sum(int a, int b) {
+        System.out.println("Sum is: " + (a + b));
+    }
+
+    public static void main(String[] args) {
+        int a = 5, b = 10;
+        sum(a, b);
+    }
+}
