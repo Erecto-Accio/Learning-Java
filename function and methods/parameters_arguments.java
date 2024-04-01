fed105377ab55e1615cd9def7e3fb949a0885778
Thedarkness-bd <toisiftasrik@gmail.com>
2024-04-01 22:46:24 +0600

    Arguments and parameters sample code added

---
+++ function and methods/parameters_arguments.java
@@ -0,0 +1,11 @@
+public class parameters_arguments {
+    public static int value(int a, int b) { // formal parameters
+
+        return (a + b);
+    }
+
+    public static void main(String[] args) {
+        int res = value(1, 50); // arguments
+        // print the result
+    }
+}
