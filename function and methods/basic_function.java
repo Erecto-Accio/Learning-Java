0c069fb678e80b477d7a23e704d16fab8f60619d
Thedarkness-bd <toisiftasrik@gmail.com>
2024-04-01 22:33:05 +0600

    Basic Function code added

---
+++ function and methods/basic_function.java
@@ -0,0 +1,15 @@
+public class basic_function {
+
+    // defined the function
+    public static void printHelloWorld() {
+        System.out.println("Hello,World");
+        System.out.println("Hello,World");
+        System.out.println("Hello,World");
+        System.out.println("Hello,World");
+    }
+
+    public static void main(String[] args) {
+        // calling the function
+        printHelloWorld();
+    }
+}
