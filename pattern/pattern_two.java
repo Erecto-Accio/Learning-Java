22aa5d3e8e5eb981c02f0f5e98d4b20c9f903bb7
Thedarkness-bd <toisiftasrik@gmail.com>
2024-04-01 22:22:52 +0600

    pattern two added

---
+++ pattern/pattern_two.java
@@ -0,0 +1,10 @@
+public class pattern_two {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 4; i++) {
+            for (int j = 1; j <= 5 - i + 1; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
