805fad78f08a58af2c89b5aaac9fd86686267c43
Thedarkness-bd <toisiftasrik@gmail.com>
2024-04-04 05:39:02 +0600

    binomial coefficent added

---
+++ function and methods/binomial_coeffiecient.java
@@ -0,0 +1,24 @@
+public class binomial_coeffiecient {
+    public static int factorial(int a) {
+        int fact = 1;
+        for (int i = 1; i <= a; i++) {
+            fact = fact * i;
+        }
+
+        return fact;
+    }
+
+    public static int binomialCoefficent(int n, int r) {
+        int n_fact = factorial(n);
+        int r_fact = factorial(r);
+        int n_m_r_fact = factorial(n - r);
+
+        int biCoeff = n_fact / (r_fact * n_m_r_fact);
+
+        return biCoeff;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(binomialCoefficent(5, 2));
+    }
+}
